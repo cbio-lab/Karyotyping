f475bbe10739f3c9254d4a6e92399c40cf1da8d2
Vera <147503317+kanaevavera@users.noreply.github.com>
2025-04-25 13:38:54 +0300

    Add files via upload

--- AluY_distribution.R
+++ AluY_distribution.R
@@ -1,21 +1,18 @@
-install.packages('patchwork')
-
 library(rtracklayer)
 
 library(tidyverse)
 library(dplyr)
-library(jsonlite)
 
 library(ggplot2)
-library(RColorBrewer)
 library(patchwork)
+library(gridExtra)
 
 args <- commandArgs(trailingOnly = TRUE)
 ucsc_id <- args[1]
-work_dir <- args[1]
+work_dir <- args[2]
 
 # Upload alignment
-alig <- read.csv(paste(work_dir, '/', ucsc_id, '_genome_dir/', ucsc_id, '_mapping.txt'), 
+alig <- read.csv(paste(work_dir, '/', ucsc_id, '_genome_dir/', ucsc_id, '_mapping.txt', sep = ''), 
                  sep = '\t', col.names = c('ID', 'count', 'chr', 'place', c(5:21)))
 
 # Choose the genome and connection
@@ -45,47 +42,42 @@ centromeres <- getTable(query) %>%
 chromosome_info <- left_join(chrs, centromeres, by = 'chrom') %>% 
   replace(is.na(.), 0)
 
+
 # Pull normalize coefficient
 norm_dens <- c()
-for (i in c(1:nrow(chr_info))){
+for (i in c(1:nrow(chromosome_info))){
   alig_chr <- alig %>% 
-    filter(chr == chr_info$chrom[i]) %>% 
+    filter(chr == chromosome_info$chrom[i]) %>% 
     select(chr, place) %>% 
     mutate(cnt = 1)
   
-  print(chr_info$chrom[i])
-  seps <- chr_info$lens[i]/50
-  tics <- seq(0, chr_info$lens[i], by = seps)
+  seps <- chromosome_info$lens[i]/50
+  tics <- seq(0, chromosome_info$lens[i], by = seps)
   dens <- c()
   for (k in tics){
     dens <- append(dens, sum(alig_chr %>% 
                                filter(place >= k & place < k+seps) %>% 
-                               pull(cnt)))
-  }
-  print(max(dens))
-  norm_dens <- append(norm_dens, max(dens))
-}
+                               pull(cnt)))}
+  #print(max(dens))
+  norm_dens <- append(norm_dens, max(dens))}
+
 norm_coef <- max(norm_dens)
 
 
 # Visualize chromosome FISH-colouring
-for (i in c(1:nrow(chr_info))){
-  print(chr_info$chrom[i])
+for (i in c(1:nrow(chromosome_info))){
   alig_chr <- alig %>% 
-    filter(chr == chr_info$chrom[i]) %>% 
+    filter(chr == chromosome_info$chrom[i]) %>% 
     select(chr, place) %>% 
     mutate(cnt = 1)
   
-  seps = chr_info$lens[i]/50
-  tics <- seq(0, chr_info$lens[i], by = seps)
+  seps = chromosome_info$lens[i]/50
+  tics <- seq(0, chromosome_info$lens[i], by = seps)
   dens <- c()
   for (k in tics){
-    #print(i)
     dens <- append(dens, sum(alig_chr %>% 
                                filter(place >= k & place < k+seps) %>% 
-                               pull(cnt)/norm_coef))
-  }
-  #print(dens)
+                               pull(cnt)/norm_coef))}
   
   chr_density <- as_tibble(tics) %>% 
     mutate(dens = dens)
@@ -93,17 +85,17 @@ for (i in c(1:nrow(chr_info))){
   chr_vis <- ggplot(chr_density) +
     geom_bar(aes(x = value, y = 1, fill = dens),
              stat = "identity", position = "dodge") +
-    scale_x_continuous(name = 'position', breaks = seq(0, chr_info$lens[i], by = 10000000)) +
+    scale_x_continuous(name = 'position', breaks = seq(0, chromosome_info$lens[i], by = 10000000)) +
     scale_fill_gradientn(
       colours = c("darkblue", 'springgreen3', "green1"),
       rescaler = ~ scales::rescale_mid(.x, mid = 0.3),
       limits = c(0, 1)) +
-    annotate("rect", xmin = chr_info$centStart[i], xmax = chr_info$centEnd[i], ymin = 0, ymax = 1,
+    annotate("rect", xmin = chromosome_info$centStart[i], xmax = chromosome_info$centEnd[i], ymin = 0, ymax = 1,
              alpha = 1, fill = "black") +
-    labs(title = paste('chromosome', str_split_i(chr_info$chrom[i], 'chr', -1), '\n', 'N =', nrow(alig_chr))) +
+    labs(title = paste('chromosome', str_split_i(chromosome_info$chrom[i], 'chr', -1), '\n', 'N =', nrow(alig_chr))) +
     theme_bw()
   
-  ggsave(paste(work_dir, '/plots/', ucsc_id, "_coloring_", chr_info$chrom[i], ".pdf", sep = ''),
+  ggsave(paste(work_dir, '/plots/', ucsc_id, "_coloring_", chromosome_info$chrom[i], ".pdf", sep = ''),
          chr_vis, width=6, height=2, units="in", scale=3)
 }
 
@@ -133,8 +125,7 @@ for (i in c(1:nrow(chr_info))){
   for (k in tics){
     dens <- append(dens, sum(alig_chr %>% 
                                filter(place >= k & place < k+seps) %>% 
-                               pull(cnt)/norm_coef))
-  }
+                               pull(cnt)/norm_coef))}
   chr_density <- as_tibble(tics) %>% 
     mutate(dens = dens)
   
@@ -163,6 +154,6 @@ for (i in c(1:nrow(chr_info))){
   chr_vis <- grid.arrange(chr_vis, bottom = str_split_i(chr_info$chrom[i], 'chr', -1))
   plot_list[[i]] <- chr_vis}
 
-main_plot <- wrap_plots(plot_list, ncol = 11)
+main_plot <- wrap_plots(plot_list, ncol = nrow(chromosome_info)/2)
 ggsave(paste(work_dir, '/plots/', ucsc_id, "_main.pdf", sep = ''),
        main_plot, width=5, height=4, units="in", scale=3)
